9c5c1df0fa236e62e8848235eacc95e79efa4c9b
TALKIMJUN <161567218+TALKIMJUN@users.noreply.github.com>
2024-02-28 15:07:10 +0700

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,11 @@
+name = "junaidi s.talkim"
+age  = "19"
+live  = "cikaroya"
+hobby = "sleeping"
+gender = "male"
+
+print("my name is =" ,name)
+print("my age =" ,age)
+print("i live in =" ,live)
+print("my hobby =" ,hobby)
+print("gender =" ,gender)
